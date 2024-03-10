018fa8cee93af18148df5f432752a2152061c5a8
VinzentSchmid <vinzent.schmid@edu.fh-joanneum.at>
2024-03-10 18:03:42 +0100

    added components for company student and home, and added routing for this pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/view/login.component';
+import { HomeComponent } from './pages/home/view/home.component';
+import { CompanyComponent } from './pages/company/view/company.component';
+import { StudentComponent } from './pages/student/view/student.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'home', component: HomeComponent, title: 'Startseite' },
+  { path: 'company', component: CompanyComponent, title: 'Unternehmen' },
+  { path: 'student', component: StudentComponent, title: 'Studenten' },
   // TODO: Add the routes here for componentss
-
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
