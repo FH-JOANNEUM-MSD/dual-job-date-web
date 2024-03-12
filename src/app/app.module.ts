d9bf126b134b472916d4d6d3018a5aa8bd298c87
Julian Kapellari <julian.kapellari@edu.fh-joanneum.at>
2024-03-12 10:02:46 +0100

    fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/view/login.component';
-import { TranslateModule } from '@ngx-translate/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './pages/login/view/login.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {HomeComponent} from "./pages/home/view/home.component";
+import {CompanyComponent} from "./pages/company/view/company.component";
+import {StudentComponent} from "./pages/student/view/student.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent, CompanyComponent, StudentComponent],
   imports: [BrowserModule, AppRoutingModule, TranslateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
