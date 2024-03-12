99d5fc8efdf8d8c6bbaf71301bb794d029c30c3b
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2024-03-12 15:19:10 +0100

    [TASK] added Auth Gard [TASK] added auth Service [TASK] made some styling in the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -12,7 +14,7 @@ import {StudentComponent} from "./pages/student/view/student.component";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent, CompanyComponent, StudentComponent],
-  imports: [BrowserModule, AppRoutingModule, TranslateModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, TranslateModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/login/view/login.component.ts
+++ src/app/pages/login/view/login.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {LoginService} from "../core/login.service";
+import { FormGroup, FormControl } from '@angular/forms';
+import {AuthService} from "../core/auth.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,26 @@ import {LoginService} from "../core/login.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  
-  constructor(private loginService: LoginService) {
+
+  constructor(private authService: AuthService,private router: Router) {
+  }
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+
+    if(typeof username === 'string' && typeof password === 'string'){
+      this.authService.login(username,password)
+    }
+
+
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/home']);
+    }
   }
 }
