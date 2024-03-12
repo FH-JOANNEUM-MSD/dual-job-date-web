6441c44906ea4da3c4c8e68c8aaba5e3ffd9e29f
Julian Kapellari <julian.kapellari@edu.fh-joanneum.at>
2024-03-12 10:10:04 +0100

    added falback route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/view/login.component';
-import { HomeComponent } from './pages/home/view/home.component';
-import { CompanyComponent } from './pages/company/view/company.component';
-import { StudentComponent } from './pages/student/view/student.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './pages/login/view/login.component';
+import {HomeComponent} from './pages/home/view/home.component';
+import {CompanyComponent} from './pages/company/view/company.component';
+import {StudentComponent} from './pages/student/view/student.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, title: 'Login' },
-  { path: 'home', component: HomeComponent, title: 'Startseite' },
-  { path: 'company', component: CompanyComponent, title: 'Unternehmen' },
-  { path: 'student', component: StudentComponent, title: 'Studenten' },
+  {path: 'login', component: LoginComponent, title: 'Login'},
+  {path: 'home', component: HomeComponent, title: 'Startseite'},
+  {path: 'company', component: CompanyComponent, title: 'Unternehmen'},
+  {path: 'student', component: StudentComponent, title: 'Studenten'},
 
   // TODO: Add the routes here for components
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
