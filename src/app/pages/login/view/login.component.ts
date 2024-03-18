6f00af1898f6d5cbc25581c574403b737850aa18
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2024-03-18 08:04:51 +0100

    [TASK] changed Auth Service Location

--- src/app/pages/login/view/login.component.ts
+++ src/app/pages/login/view/login.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import {AuthService} from "../core/auth.service";
+import {AuthService} from "../../../Services/auth.service";
 import { Router } from '@angular/router';
 
 @Component({
