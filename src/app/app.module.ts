27f8f63e99f54905135fc3aca3ab21d1e71d55f7
Julian Kapellari <julian.kapellari@gmail.com>
2024-04-23 10:15:04 +0200

    started with companies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import {StudentDialogComponent} from './dialogs/student-dialog/student-dialog.co
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {CompanyDialogComponent} from "./dialogs/company-dialog/company-dialog.component";
+import {CompanyProfileComponent} from "./pages/company-profile/view/company-profile.component";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -39,7 +40,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     StudentComponent,
     HeadernavigationComponent,
     StudentDialogComponent,
-    CompanyDialogComponent
+    CompanyDialogComponent,
+    CompanyProfileComponent
   ],
   imports: [
     BrowserModule,
