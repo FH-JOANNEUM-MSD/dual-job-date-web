db15804351f5e2e8b45a3ace87ec78d423b2a660
ServerKnights <stefan.bicha1@gmx.at>
2024-05-28 22:07:26 +0200

    [TASk] Lets you view the current Page in the nav

--- src/app/custom-components/header-navigation/view/header-navigation.component.ts
+++ src/app/custom-components/header-navigation/view/header-navigation.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { UserType } from 'src/app/core/enum/userType';
 import { DialogService } from '../../../services/dialog.service';
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-headernavigation',
@@ -11,6 +12,7 @@ import { DialogService } from '../../../services/dialog.service';
 })
 export class HeadernavigationComponent implements OnInit {
   navLinks: any[] = [];
+  currentPage: string | undefined;
 
   ngOnInit(): void {
     const userType = this.authService.getUserType();
@@ -24,7 +26,10 @@ export class HeadernavigationComponent implements OnInit {
         { path: '/student', label: 'Studenten' },
       ];
     }
+
+    this.updatePageTitle(this.router.url);
   }
+
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -38,5 +43,29 @@ export class HeadernavigationComponent implements OnInit {
 
   openChangePasswordDialog(): void {
     this.dialogService.openChangePasswordDialog().subscribe();
+  }
+
+  updatePageTitle(url: string) {
+    // Here you can add logic to map URLs to human-readable page names
+    if (url.startsWith('/company-profile/')) {
+      this.currentPage = 'Unternehmensprofil';
+    } else {
+      // Map URLs to human-readable page names
+      switch (url) {
+        case '/home':
+          this.currentPage = 'Startseite';
+          break;
+        case '/company':
+          this.currentPage = 'Unternehmen';
+          break;
+        case '/student':
+          this.currentPage = 'Studenten';
+          break;
+        // Add other cases as needed
+        default:
+          this.currentPage = '404';
+          break;
+      }
+    }
   }
 }
